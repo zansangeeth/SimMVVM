9ad45efddf0e3debaa7979ee0c1cad7bfa939c18
zansangeeth <zansangeeth17@gmail.com>
2022-08-05 01:57:49 +0530

    Initial commit

---
+++ app/src/main/java/com/zasa/simmvvm/QuoteActivity.kt
@@ -0,0 +1,11 @@
+package com.zasa.simmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote)
+    }
+}
